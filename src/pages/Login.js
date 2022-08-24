49a4758613f9a4b570298e7b9ad1cb8e9a86ac11
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-24 16:08:06 +0530

    Login UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,21 @@
+import styles from '../styles/login.module.css'
 const Login = () => {
-    return <h1>Login</h1>
+    return (
+        <form className= {styles.loginForm}>
+            <span className={styles.loginSignupHeader} >
+                LogIn
+            </span>
+            <div className={styles.field}>
+                <input type='email' placeholder='Email' required />
+            </div>
+            <div className={styles.field}>
+                <input type='password' placeholder='Password' required />
+            </div>
+            <div className={styles.field}>
+                <button>LogIn</button>
+            </div>
+        </form>
+    )
 }
 
 export default Login 
