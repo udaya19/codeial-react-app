6187e096f813011799e44a5241d4959dca34dcdb
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-24 16:45:11 +0530

    Added react toast notifications

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './styles/index.css';
 import './styles/index.css'
+import { ToastProvider } from 'react-toast-notifications';
 import {App} from './components';
 import {BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <ToastProvider autoDismiss autoDismissTimeout={5000} placement='top-left'>
+          <App />
+        </ToastProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,35 @@
 import styles from '../styles/login.module.css'
+import { useState } from 'react';
+import { useToasts } from 'react-toast-notifications';
 const Login = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [loggingIn,setLoggingIn] = useState(false);
+    const {addToast} = useToasts();
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setLoggingIn(true);
+        if(!email || !password){
+            return addToast("Please enter both email and password",{
+                appearence:'error',
+            })
+        }
+    }
     return (
-        <form className= {styles.loginForm}>
+        <form className= {styles.loginForm} onSubmit={handleSubmit}>
             <span className={styles.loginSignupHeader} >
                 LogIn
             </span>
             <div className={styles.field}>
-                <input type='email' placeholder='Email' required />
+                <input type='email' placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)} />
             </div>
             <div className={styles.field}>
-                <input type='password' placeholder='Password' required />
+                <input type='password' placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}  />
             </div>
             <div className={styles.field}>
-                <button>LogIn</button>
+                <button disabled={loggingIn} >
+                    {loggingIn?'Logging in..':'LogIn'}
+                </button>
             </div>
         </form>
     )
