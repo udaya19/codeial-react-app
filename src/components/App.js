ce7f7ee3d00305fb1d5b45ae8f62e8e4df15a7f2
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-24 09:24:32 +0530

    Creating home page and rendering posts from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPosts } from '../api';
-
+import {Home} from '../pages';
+import {Loader} from './';
 function App() {
+  const [posts,setPosts] = useState([]); 
+  const [loading,setLoading] = useState(true); 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
       console.log('response', response);
+      if(response.success){
+        setPosts(response.data.posts);
+      }
+      setLoading(false);
     };
 
     fetchPosts();
   }, []);
 
+  if(loading){
+    return <Loader />
+  }
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Home posts={posts} />
     </div>
   );
 }
