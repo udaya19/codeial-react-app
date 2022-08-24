0c96fc8c2278063857f193bee73361908cdddd53
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-24 17:00:59 +0530

    Login API

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,30 @@
 import styles from '../styles/login.module.css'
 import { useState } from 'react';
 import { useToasts } from 'react-toast-notifications';
+import { login } from '../api';
 const Login = () => {
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     const [loggingIn,setLoggingIn] = useState(false);
     const {addToast} = useToasts();
-    const handleSubmit = (e)=>{
+    const handleSubmit = async (e)=>{
         e.preventDefault();
         setLoggingIn(true);
         if(!email || !password){
             return addToast("Please enter both email and password",{
+                appearence:'error',
+            })
+        }
+        const response = await login(email,password);
+        if(response.success){
+            setLoggingIn(false);
+            return addToast("Successfully logged in",{
+                appearence:'success',
+            })
+        }
+        else{
+            setLoggingIn(false);
+            return addToast(response.message,{
                 appearence:'error',
             })
         }
